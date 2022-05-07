b31c3a78ed40b2303efb6efcf25b45bee8279722
fermelli <luisfernandosalgadomiguez@gmail.com>
2022-05-07 04:26:22 -0400

    Changes for v2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,75 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import CardFeature from './components/CardFeature.vue'
+export default {
+    name: 'App',
+    components: {
+        CardFeature
+    },
+    data() {
+        return {
+            features: [
+                {
+                    title: 'Goal',
+                    count: 0,
+                    image: {
+                        src: 'goal.png',
+                        alt: 'Goal'
+                    }
+                },
+                {
+                    title: 'Idea',
+                    count: 0,
+                    image: {
+                        src: 'idea.png',
+                        alt: 'Idea'
+                    }
+                },
+                {
+                    title: 'Performance',
+                    count: 0,
+                    image: {
+                        src: 'performance.png',
+                        alt: 'Performance'
+                    }
+                },
+                {
+                    title: 'Target',
+                    count: 0,
+                    image: {
+                        src: 'target.png',
+                        alt: 'Target'
+                    }
+                }
+            ]
+        }
+    }
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <div class="container">
+        <h1 class="title">Features</h1>
+        <div class="container-cards">
+
+            <CardFeature v-for="feature in features" :feature="feature" @add-to-count="feature.count++" />
+
+        </div>
+    </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+.title {
+    color: #ffffff;
+}
+
+.container-cards {
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: repeat(4, minmax(200px, 25%));
 }
 </style>
---
+++ src/components/CardFeature.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+    name: 'CardFeature',
+    props: {
+        feature: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="card__container-image">
+            <img class="card__image" :src="`./src/assets/${feature.image.src}`" :alt="feature.image.alt">
+        </div>
+        <h2 class="card__title">{{ feature.title }} ({{ feature.count }})</h2>
+        <button class="card__button" @click="$emit('add-to-count')">Vote!</button>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    background-color: #121227;
+    padding: 3rem 1rem 2rem;
+    border-radius: 0.25rem;
+}
+
+.card__container-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #e1e1ff;
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    margin: 0 auto;
+}
+
+.card__image {
+    display: block;
+    width: 60%;
+}
+
+.card__title {
+    color: #ffffff;
+    font-weight: 300;
+    text-align: center;
+}
+
+.card__button {
+    display: block;
+    font-family: 'Poppins', sans-serif;
+    background-color: #21bbac;
+    padding: 0.5rem 1rem;
+    min-width: 120px;
+    font-weight: 500;
+    margin: 0 auto;
+    cursor: pointer;
+    text-transform: uppercase;
+}
+
+.card__button:hover {
+    background-color: #0b9c8e;
+    transition: background-color 0.3s ease;
+}
+</style>
